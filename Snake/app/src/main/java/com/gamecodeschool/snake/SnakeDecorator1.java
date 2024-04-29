e21fa52c283c06828f515e00328c8b354da0382c
TigEash <shade.death.shade+1@gmail.com>
2024-04-28 21:40:09 -0700

    Added  a TODO
    See title

--- Snake/app/src/main/java/com/gamecodeschool/snake/SnakeDecorator1.java
+++ Snake/app/src/main/java/com/gamecodeschool/snake/SnakeDecorator1.java
@@ -4,7 +4,7 @@ import android.content.Context;
 import android.graphics.Point;
 
 public class SnakeDecorator1 extends Snake{
-
+//TODO name it supersnake or something along the lines?
     private final int health;
 
     SnakeDecorator1(Context context, Point mr, int ss) {
